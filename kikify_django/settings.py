1bc0db8abd72444193b291c2ab3ef7a66838609c
Kristijan Stepanov <34355704+stepanov1997@users.noreply.github.com>
2021-03-17 23:11:48 +0100

    Update settings.py

--- kikify_django/settings.py
+++ kikify_django/settings.py
@@ -55,7 +55,7 @@ ALLOWED_HOSTS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "kikify/static"),
 )
 
 MEDIA_URL = '/media/'
